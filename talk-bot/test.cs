fb783576ebb0807801d4e94081f742e3c61853d2
Ben <ben.garrett.porter@gmail.com>
2026-08-14 18:54:41 -0400

    renamed project, updated to .NET 10 Framework, fixed broken test path

---
+++ talk-bot/test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace talk
+{
+    internal class Test(IWebDriver driver)
+    {
+        private IWebDriver _driver = driver;
+
+        public void Initialize()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl("https://github.com/nuiben/talk-bot/blob/main/README.md");
+        }
+
+        public void ExecuteTest()
+        {
+            IWebElement element = _driver.FindElement(By.Name("inputDiv"));
+            element.SendKeys("some keys");
+        }
+
+        public void ClearMemory()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
